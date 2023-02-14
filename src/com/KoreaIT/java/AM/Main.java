7ca1c9371e997f0eec1be4d67ab59431d5d8c30d
hwshin1 <hwshin5044@gmail.com>
2023-02-14 11:48:01 +0900

    무한루프로 입력받기

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -7,10 +7,15 @@ class Main {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("명령어 ) ");
-		String cmd = sc.nextLine();
-		cmd = sc.nextLine();
-		System.out.printf("입력된 명령어 : %s\n", cmd);
+		
+		while (true) {
+			System.out.println("명령어 ) ");
+			String cmd = sc.nextLine();
+			
+			if (cmd.equals("system exit")) {
+				break;
+			}
+		}
 		
 		System.out.println("== 프로그램 끝 ==");
 		sc.close();
