75cdb1716d6d65bd52508064479aa9f6ed7860e9
hwshin1 <hwshin5044@gmail.com>
2023-02-17 10:11:05 +0900

    명령어 연습 및 게시물 작성 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -7,13 +7,37 @@ class Main {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
+		int ArticleId = 0;
 		
 		while (true) {
 			System.out.println("명령어 ) ");
 			String cmd = sc.nextLine();
 			
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
 			if (cmd.equals("system exit")) {
 				break;
+			}
+			
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} 
+			else if (cmd.equals("article write")) {
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%s / %s\n", title, body);
+				
+				System.out.println((ArticleId + 1) + "번 글이 생성되었습니다");
+				ArticleId++;
+			} 
+			else {
+				System.out.println("존재하지 않습니다");
 			}
 		}
 		
