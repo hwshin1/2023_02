8a1d08465515b0b56604f5e06bb06c8467796d15
hwshin1 <hwshin5044@gmail.com>
2023-02-20 09:39:23 +0900

    게시물 목록 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -14,7 +14,7 @@ class Main {
 		
 		while (true) {
 			System.out.println("명령어 ) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 			
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
@@ -28,29 +28,28 @@ class Main {
 			if (cmd.equals("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
-					continue;
+//					continue;
+				} else {
+					System.out.println("  번호  /  제목  ");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("  %d  /  %s  \n", article.id, article.title);
+					}
 				}
-				else {
-					System.out.println("게시글 있음");
-				}
-			} 
-			else if (cmd.equals("article write")) {
+			} else if (cmd.equals("article write")) {
 				int id = ArticleId + 1;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-//				System.out.printf("%s / %s\n", title, body);
-				
 				Article article = new Article(id, title, body);
 				articles.add(article);
 				
 				System.out.println(id + "번 글이 생성되었습니다");
 				ArticleId++;
-			} 
-			else {
-				System.out.println("존재하지 않습니다");
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
 		
