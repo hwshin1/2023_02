328a4fb7a9083a4024a1e03d5f4833d6c07fab07
hwshin1 <hwshin5044@gmail.com>
2023-02-20 10:21:38 +0900

    게시물 상세보기 구현(글의 유무 판단)

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -28,7 +28,6 @@ class Main {
 			if (cmd.equals("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
-//					continue;
 				} else {
 					System.out.println("  번호  /  제목  ");
 					for (int i = articles.size() - 1; i >= 0; i--) {
@@ -48,6 +47,28 @@ class Main {
 				
 				System.out.println(id + "번 글이 생성되었습니다");
 				ArticleId++;
+				
+			} else if (cmd.startsWith("article detail ")) {
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+				
+				boolean found = false;
+				
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						found = true;
+						break;
+					}
+				}
+				
+				if (found == false) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				
+				System.out.printf("%d번 글 있던데?\n", id);
+				
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
