949b8e7d9f6c4438c99a03af502fbc695ac1ccdd
hwshin1 <hwshin5044@gmail.com>
2023-02-21 10:50:08 +0900

    게시물 수정 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -73,6 +73,34 @@ class Main {
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 				
+			} else if (cmd.startsWith("article modify ")) {
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+				Article foundArticle = null;
+				
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if(foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				foundArticle.title = title;
+				foundArticle.body = body;
+				
+				System.out.printf("%d번 글을 수정했습니다\n", id);
+				
 			} else if (cmd.startsWith("article delete ")) {
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
@@ -92,9 +120,10 @@ class Main {
 				}
 				
 				articles.remove(foundIndex);
+				
 				System.out.printf("%d번 글을 삭제 했습니다\n", id);
-			}
-			else {
+				
+			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
